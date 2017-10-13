4f69dfe5da7abdcb17731b20499f35f2540038e2
Danil P <slemonide@gmail.com>
2017-10-12 18:21:08 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function main() {
+    var canvas = document.getElementById('main');
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
+}
